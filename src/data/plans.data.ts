b45c321220b8b182b23aac85f89cc880195f9621
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-03-29 11:33:33 -0300

    Add plans.data.ts and update dependencies

---
+++ src/data/plans.data.ts
@@ -0,0 +1,33 @@
+const PLAN_ID_PREFIX = "plan-";
+
+type PlansProps = {
+  id: string;
+  name: string;
+  price: number;
+  features: string[];
+  bestChoice?: boolean;
+};
+
+export const PLANS: PlansProps[] = [
+  {
+    id: `${PLAN_ID_PREFIX}1`,
+    name: "Basic",
+    price: 10,
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    bestChoice: false,
+  },
+  {
+    id: `${PLAN_ID_PREFIX}2`,
+    name: "Standard",
+    price: 20,
+    features: ["Feature 1", "Feature 2", "Feature 3", "Feature 4"],
+    bestChoice: true,
+  },
+  {
+    id: `${PLAN_ID_PREFIX}3`,
+    name: "Premium",
+    price: 30,
+    features: ["Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5"],
+    bestChoice: false,
+  },
+];
